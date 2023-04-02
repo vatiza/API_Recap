8bc0d93bf5c97f942ddc67e08696310f6f2d3f27
vatiza <rakibur.mac@gmail.com>
2023-04-02 15:44:24 +0600

    serch field add

--- js/app.js
+++ js/app.js
@@ -1,13 +1,38 @@
-const loadPhone = async () => {
-  const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadPhone = async (searchText) => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
+  displayPhone(data.data);
 };
 
 const displayPhone = (phones) => {
   const phoneContainer = document.getElementById("phone-container");
-  phones.forEach((phone) => {});
+  phoneContainer.textContent='';
+  phones.forEach((phone) => {
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add("col");
+    phoneDiv.innerHTML = `
+    <div class="card">
+    <img src="${phone.image}" class="card-img-top p-4" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">${phone.phone_name}</h5>
+      <p class="card-text">
+        This is a longer card with supporting text below as a natural
+        lead-in to additional content. This content is a little bit
+        longer.
+      </p>
+    </div>
+  </div> 
+   
+  `;
+    phoneContainer.appendChild(phoneDiv);
+  });
 };
+ 
+document.getElementById('btn-search').addEventListener('click',function(){
+  const searchField=document.getElementById('search-input');
+  const searchText=searchField.value;
+loadPhone(searchText);
+})
 
 loadPhone();
